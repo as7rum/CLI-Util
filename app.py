dc78585ceb172edaa6eac31b1387f94203f273f5
as7rum <as7rum@MacBook-as7rum.local>
2023-05-26 13:26:23 +0300

    finish all tecnical isuses

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from docopt  import docopt
+from pprint import pprint
 from progress.bar import ShadyBar
 import random
 
@@ -12,7 +13,7 @@ Builds and Tasks CLI.
 Usage:
     app.py test
     app.py list <file_name>
-    app.py get <file_name> <task_or_build_name>
+    app.py get <task_or_build> <task_or_build_name>
 '''
     # Передаем регистрацию в docopt для отлавливания запросов в командной строке
     args = docopt(usage)
@@ -49,32 +50,52 @@ Usage:
     # Формируем команду list для вывода списка Задач или Билдов
     if args['list']:
         try:
-            service = FileService(f'data/{args["<file_name>"]}.yaml')
+            service = FileService()
             print(f'List of available {args["<file_name>"]}:')
-            print(*[f' * {item} \n' for item in service.get_list_of_names()], end='')
+            print(*[f' * {item} \n' for item in service.get_list_of_names(
+                f'data/{args["<file_name>"]}.yaml')], end='')
         except:
             print(
     '''The file does not exist or the request was entered incorrectly. Try again!''')
 
-    # Формируем команду get для получения более подробной информации 
+    # Формируем команды get для получения более подробной информации 
     # по конкретной Задаче или Билду
 
     if args['get']:
-        try:
-            service = FileService(f'data/{args["<file_name>"]}s.yaml')
-            item = service.get_item_by_name(args["<task_or_build_name>"])
-            if item:
-                what_is_list = next(filter(lambda i: type(item[i]) is list, item), None)
-                item[what_is_list] = ', '.join(item[what_is_list])
-                print(f'{args["<file_name>"].title()} info:')
-                print(*[f' * {i}: {item[i]}\n' for i in item], end='')
-            else:
+        service = FileService()
+        item = None
+        if args['<task_or_build>'] == 'task':
+            try:
+                item = service.get_task_dependencies(args["<task_or_build_name>"])
+            except:
                 print(
-    '''Task or Build does not exist or the Name was entered incorrectly. Try again!''')
-        except:
+        '''Task does not exist or the request was entered incorrectly. Try again!''')
+        if args['<task_or_build>'] == 'build':
+            try:
+                item = service.get_builds_with_tasks_and_their_dep(args["<task_or_build_name>"])
+            except:
+                print(
+        '''Build does not exist or the request was entered incorrectly. Try again!''')
+        if item:
+            item_name = item['name']
+            what_is_list = next(filter(lambda i: type(item[i]) is list, item), None)
+            print(f'{args["<task_or_build>"].title()} info:')
+            print(f'* name: {item_name}')
+            print(f'* {what_is_list}:')
+            for i in item[what_is_list]:
+                print_task(i)
+        else:
             print(
-    '''Item does not exist or the request was entered incorrectly. Try again!''')
+    '''Request was entered incorrectly.''')
 
+# Рекурсивный принт для красивого вывода
+def print_task(task: dict, level=0):
+    indent = "|" + "   " * level
+    task_name = task['name']
+    what_is_list = next(filter(lambda t: type(task[t]) is list, task), None)
+    print(f'{indent}* {task_name}')
+    for dependency in task[what_is_list]:
+        print_task(dependency, level + 1)
 
 if __name__ == '__main__':
     main()
--- file_service.py
+++ file_service.py
@@ -7,29 +7,46 @@ class FileService:
     получения более детальной информации по каждому из них.
     """
 
-    def __init__(self, file: str) -> None:
+    def init(self, file: str) -> None:
         self.file = file
     
-    def get_list_of_names(self) -> list:
+    def get_list_of_names(self, file) -> list:
         """
         Получаем список для просмотра имен 
         загруженных билдов и задач соответственно.
         """
-        list_with_tasks_or_builds = self.__get_list_of_items_from_yaml_file()
+        list_with_tasks_or_builds = self.__get_list_of_items_from_yaml_file(file)
         return list(map(lambda x: x.get('name'), list_with_tasks_or_builds))
     
-    def get_item_by_name(self, item_name: str) -> dict:
+    def get_builds_with_tasks_and_their_dep(self, build_name: str) -> dict:
         """
         Команда get для получения детальной информации
-        в формате словаря о какой-то конкретной задаче
-        или билде.
+        в формате словаря о каком-то билде с учетом зависимостей.
         """
-        list_with_tasks_or_builds = self.__get_list_of_items_from_yaml_file()
-        return next(filter(lambda i: i.get('name') == item_name, list_with_tasks_or_builds), None)
+        builds_data = self.__get_list_of_items_from_yaml_file('data/builds.yaml')
+        build_dict = next(filter(lambda i: i.get('name') == build_name, builds_data), None)
+        build_tasks_list = next(build['tasks'] for build in builds_data if build['name'] == build_name)
+        # Рекурсивный поиск зависимостей
+        build_dict['tasks'] = list(map(lambda task_name: self.get_task_dependencies(task_name), build_tasks_list))
+        return build_dict
+        
+    def get_task_dependencies(self, task_name) -> dict:
+        """
+        Команда get для получения детальной информации
+        в формате словаря о задаче с учетом зависимостей.
+        """
+        task = next(filter(lambda t: t['name'] == task_name, 
+                           self.__get_list_of_items_from_yaml_file('data/tasks.yaml')), None)
+        dependencies = task.get('dependencies', [])
+        return {
+        'name': task['name'],
+        'dependencies': list(map(lambda dep: self.get_task_dependencies(dep), dependencies))
+    }
+    
 
-    def __get_list_of_items_from_yaml_file(self) -> list:
+    def __get_list_of_items_from_yaml_file(self, file) -> list:
         """
-        Открываем файл . yaml и преобразуем его в список словарей
+        Открываем файл .yaml и преобразуем его в список словарей
         """
-        with open(self.file) as f:
+        with open(file) as f:
             return list(yaml.safe_load(f).values())[0]
